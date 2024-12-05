81dbcdb7a7f30fb03447324d7f1365db1b7571dd
k-uchino <k-uchino@systems-inc.co.jp>
2024-12-05 15:01:34 +0900

    2024\12\5　first commit

---
+++ practice1.java
@@ -0,0 +1,10 @@
+package myproj;
+import java.util.Scanner;
+
+public class practice1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt(); // 入力部分
+		System.out.println(N * N); // 出力部分
+	}
+}
